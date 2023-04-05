5a8deb21f7598fc0451abc94c513e1dbf3defa61
grigorydobrenko <grigorydobrenko97@gmail.com>
2023-04-05 14:08:16 +0300

    style: stylize buttons

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,9 +5,11 @@ import ChartsPage from "../pages/ChartsPage";
 
 function App() {
     const dispatch = useAppDispatch()
+
     useEffect(() => {
         dispatch(getChartsTC())
     }, [dispatch])
+
     return (
         <div>
             <ChartsPage/>
--- src/layouts/header/Header.tsx
+++ src/layouts/header/Header.tsx
@@ -1,23 +1,33 @@
 import React from 'react';
 import styles from './Header.module.css'
-import {useAppDispatch} from "../../hooks/hooks";
+import {useAppDispatch, useAppSelector} from "../../hooks/hooks";
 import {SelectedYearType, setYearAC} from "../../store/reducers/charts-reducer";
+import cn from 'classnames';
+
 
 const Header = () => {
 
     const dispatch = useAppDispatch()
+    const year = useAppSelector(state => state.charts.selected_year)
 
     const onClickHandler = (year: SelectedYearType) => {
         dispatch(setYearAC(year))
     }
 
-
     return (
         <header className={styles.header_container}>
-            <button className={styles.btn} onClick={() => onClickHandler('2021')}>2021</button>
-            <button className={styles.btn} onClick={() => onClickHandler('2022')}>2022</button>
-            <button className={styles.btn} onClick={() => onClickHandler('2023')}>2023</button>
-            <button className={styles.btn} onClick={() => onClickHandler('2024')}>2024</button>
+            <button className={cn(styles.btn, {[styles.btn_active]: year === '2021'})}
+                    onClick={() => onClickHandler('2021')}>2021
+            </button>
+            <button className={cn(styles.btn, {[styles.btn_active]: year === '2022'})}
+                    onClick={() => onClickHandler('2022')}>2022
+            </button>
+            <button className={cn(styles.btn, {[styles.btn_active]: year === '2023'})}
+                    onClick={() => onClickHandler('2023')}>2023
+            </button>
+            <button className={cn(styles.btn, {[styles.btn_active]: year === '2024'})}
+                    onClick={() => onClickHandler('2024')}>2024
+            </button>
         </header>
     );
 };
